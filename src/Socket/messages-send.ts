8022863ce5a78a5897a81f49f15b3189eeb0d4b7
mmhospedagem <contato@mmhospedagem.com.br>
2024-09-06 13:46:01 -0300

    teste

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -355,7 +355,7 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 	const relayMessage = async(
 		jid: string,
 		message: proto.IMessage,
-		{ messageId: msgId, participant, additionalAttributes, additionalNodes, useUserDevicesCache, cachedGroupMetadata, useCachedGroupMetadata, statusJidList }: MessageRelayOptions
+		{ messageId: msgId, participant, additionalAttributes, additionalNodes, useUserDevicesCache, useCachedGroupMetadata, statusJidList }: MessageRelayOptions
 	) => {
 		const meId = authState.creds.me!.id
 
